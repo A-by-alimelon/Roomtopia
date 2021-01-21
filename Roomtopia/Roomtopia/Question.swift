58199667348ea99b55bb915473fe3831e28beb60
alimelon <alimelon97@gmail.com>
2021-01-21 14:00:54 +0900

    [feat] Question, Answer 객체 추가

---
+++ Roomtopia/Roomtopia/Question.swift
@@ -0,0 +1,18 @@
+//
+//  Question.swift
+//  Roomtopia
+//
+//  Created by A on 2021/01/21.
+//
+
+import Foundation
+
+class Question {
+    var text: String = ""
+    var answer: Answer?
+}
+
+
+enum Answer: Int {
+    case veryYes = 5, yes = 4, normal = 3, no = 2, veryNo = 1
+}
