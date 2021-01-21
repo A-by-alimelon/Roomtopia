d5123a97de475e407d89419f435137ee04864726
alimelon <alimelon97@gmail.com>
2021-01-22 00:15:50 +0900

    [feat] progressView 로직 추가

--- Roomtopia/Roomtopia/ViewController.swift
+++ Roomtopia/Roomtopia/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var surveyTableView: UITableView!
     @IBOutlet weak var progressView: UIProgressView!
+    @IBOutlet weak var progressLabel: UILabel!
     
     let items = [Question("한 번 사는 인생, 재밌게 살고 싶다."), Question("나는 여행을 가면 사진을 꼭 찍어 sns에 글을 올린다."),Question("나는 인싸다."), Question("나는 문화생활을 위해 쓰는 돈이 별로 아깝지 않다."), Question("나는 약속이 있는 다음 날 집에서 쉰다."), Question(" 나는 낯선 사람들과 있어도 금방 잘 어울린다."), Question("시험기간에 나는 친구들과 모여서 함께 공부한다."), Question("간짜장을 시켰는데 일반 짜장이 배달오자 배달원에게 다시 가져가 달라고 한다."), Question("맛집을 찾아간 나는 비싼 가격에 놀라 다른 가게로 간다."), Question("나는 계획이 없는 편이다.")]
     
@@ -19,8 +20,12 @@ class ViewController: UIViewController {
             if count == 10 {
                 print("다해")
             }
+            progress.completedUnitCount = Int64(count)
+            progressView.setProgress(Float(progress.fractionCompleted), animated: true)
+            progressLabel.text = "\(count*10)%"
         }
     }
+    let progress = Progress(totalUnitCount: 10)
     
     
     
@@ -31,7 +36,8 @@ class ViewController: UIViewController {
         surveyTableView.dataSource = self
         surveyTableView.allowsSelection = false
         configureFooter()
-        
+        progress.completedUnitCount = 0
+        progressLabel.text = "0%"
         NotificationCenter.default.addObserver(self, selector: #selector(changeQuestionAnswer), name: .changeAnswer, object: nil)
     }
     
