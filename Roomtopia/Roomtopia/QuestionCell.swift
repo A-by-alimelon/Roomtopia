6ff10cc07537d7f7b0939844d7b6c78e7b7cd011
alimelon <alimelon97@gmail.com>
2021-01-21 15:13:15 +0900

    [feat] customCell 파일 추가

---
+++ Roomtopia/Roomtopia/QuestionCell.swift
@@ -0,0 +1,15 @@
+//
+//  QuestionCell.swift
+//  Roomtopia
+//
+//  Created by A on 2021/01/21.
+//
+
+import UIKit
+
+class QuestionCell: UITableViewCell {
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var answerStackView: UIStackView!
+    
+  
+}
